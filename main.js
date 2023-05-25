5dd4542d94b3aa28def3e7341b4c88d567eebf8b
Bozzay Viktor <bozzay.viktor@gmail.com>
2023-05-25 19:47:16 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,13 +2,6 @@ const video = document.getElementById("video");
 const app = document.getElementById("app");
 
 window.addEventListener("load", () => {
-  Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
-    faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
-    faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
-    faceapi.nets.faceExpressionNet.loadFromUri("/models"),
-  ]).then(startVideo);
-
   function startVideo() {
     navigator.getUserMedia(
       {video: {}},
@@ -16,6 +9,13 @@ window.addEventListener("load", () => {
       (err) => console.error(err),
     );
   }
+
+  Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri("./models"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("./models"),
+    faceapi.nets.faceRecognitionNet.loadFromUri("./models"),
+    faceapi.nets.faceExpressionNet.loadFromUri("./models"),
+  ]).then(startVideo);
 
   video.addEventListener("play", () => {
     const canvas = faceapi.createCanvasFromMedia(video);
